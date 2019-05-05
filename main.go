57fce2bb4ee23310f34c301d4448710cd00d44e5
Adam Alpern <adam.alpern@gmail.com>
2019-05-04 18:41:18 -0700

    Update to dump data in approximately D3-compatible format

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 
 	cli "github.com/jawher/mow.cli"
 	log "github.com/sirupsen/logrus"
@@ -58,10 +59,14 @@ func (s *FileStats) Add(info os.FileInfo) {
 	}
 	size := info.Size()
 	mtime := info.ModTime()
+	year := mtime.Year()
+	if year < 2002 {
+		year = 2002
+	}
 	s.FileCount += 1
 	s.TotalSize += size
-	s.SizeByYear[mtime.Year()] = s.SizeByYear[mtime.Year()] + size
-	yd := s.getYearDayMap(mtime.Year())
+	s.SizeByYear[year] = s.SizeByYear[year] + size
+	yd := s.getYearDayMap(year)
 	yd[mtime.YearDay()] = yd[mtime.YearDay()] + size
 }
 
@@ -90,16 +95,50 @@ func (s *FileStats) Walk(dir string) error {
 	return err
 }
 
-func (s *FileStats) Dump() {
-	log.WithFields(log.Fields{
-		"action":           "Dump",
-		"file_count":       s.FileCount,
-		"dir_count":        s.DirCount,
-		"total_size_bytes": s.TotalSize,
-		"year_stats":       s.SizeByYear,
-	}).Info()
+// ----------------------------------------------------------------------
+// Dumping to D3 datum compatible format for graphing
+// ----------------------------------------------------------------------
 
-	if js, err := json.Marshal(s); err != nil {
+type D3Year struct {
+	Year    int     `json:"year"`
+	Bytes   int64   `json:"bytes"`
+	KB      float64 `json:"kb"`
+	MB      float64 `json:"mb"`
+	GB      float64 `json:"gb"`
+	GBTotal float64 `json:"gbtotal"`
+}
+
+type D3Years []D3Year
+
+func (a D3Years) Len() int           { return len(a) }
+func (a D3Years) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a D3Years) Less(i, j int) bool { return a[i].Year < a[j].Year }
+
+type D3YearDump struct {
+	Key    string  `json:"key"`
+	Values D3Years `json:"values"`
+}
+
+func (s *FileStats) Dump() {
+	var d3 D3YearDump
+	for year, bytes := range s.SizeByYear {
+		d3.Values = append(d3.Values, D3Year{
+			Year:  year,
+			Bytes: bytes,
+			KB:    float64(bytes) / 1024,
+			MB:    float64(bytes) / (1024 * 1024),
+			GB:    float64(bytes) / (1024 * 1024 * 1024),
+		})
+	}
+	sort.Sort(d3.Values)
+	for i, _ := range d3.Values {
+		d3.Values[i].GBTotal = d3.Values[i].GB
+		if i > 0 {
+			d3.Values[i].GBTotal += d3.Values[i-1].GBTotal
+		}
+	}
+
+	if js, err := json.MarshalIndent([]D3YearDump{d3}, "", "  "); err != nil {
 		log.WithFields(log.Fields{
 			"action": "dump",
 			"status": "json_error",
